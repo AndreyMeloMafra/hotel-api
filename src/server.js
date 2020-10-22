7d66a662c14b5c6f423581c5bc0ccb64ca9c1731
AndreyMeloMafra <andreymelo44@gmail.com>
2020-10-22 14:55:44 -0300

    heroku settings

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const cors = require('cors');
 const routes = require('./routes');
+
 
 require('./database');
 
