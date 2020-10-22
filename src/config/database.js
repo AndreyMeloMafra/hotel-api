ab58f3b69a31fe8ae019829831ac8bc922ff2042
AndreyMeloMafra <andreymelo44@gmail.com>
2020-10-22 17:02:53 -0300

    Back to ClearDB

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   username: 'b0bf77cd9403a3',
   password: '5c6984f8',
-  database: 'conheca_piranhas',
+  database: 'heroku_52e29600bd4273f',
   host: 'us-cdbr-east-02.cleardb.com',
   port: 3306,
   dialect: 'mysql',
